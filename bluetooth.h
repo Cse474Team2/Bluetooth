ce93711223653aa83daaa7f02f8d819890f84fc5
Christopher Addison <chris.cjteam@gmail.com>
2017-12-02 23:21:22 -0800

    Add library header file

---
+++ bluetooth.h
@@ -0,0 +1,16 @@
+#ifndef BLUETOOTH_H
+#define BLUETOOTH_H
+
+// Initialises bluetooth communications
+void bluetoothInit();
+
+// Sends a string via bluetooth
+void bluetoothSendString(char* str, uint32_t size);
+
+// Returns true if there is data available
+bool bluetoothCharAvailable();
+
+// Get next character from bluetooth connection
+char bluetoothGetChar();
+
+#endif // BLUETOOTH_H
